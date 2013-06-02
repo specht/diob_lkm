091673d50a5ef7233fdb9988a95862be70fb529d
Michael Specht <micha.specht@gmail.com>
2013-06-02 16:13:58 +0200

    set up hooks for open, close, lseek, read and write

--- diob.c
+++ diob.c
@@ -1,4 +1,5 @@
 /*
+ * ATTENTION: In order for this to work, we need the address of the sys_call_table:
  * grep "sys_call_table" /boot/System.map
  */
 
@@ -17,56 +18,90 @@
 
 MODULE_LICENSE("GPL");
 
-void **sys_call_table;
+void ** SYS_CALL_TABLE = (void **)0xffffffff80296f40;
+asmlinkage int (*original_open) (const char*, int, int);
+asmlinkage int (*original_close) (int);
+asmlinkage off_t (*original_lseek) (int, off_t, int);
+asmlinkage ssize_t (*original_read) (int, void*, size_t);
+asmlinkage ssize_t (*original_write) (int, const void*, size_t);
 
-asmlinkage int (*original_call) (const char*, int, int);
-
-asmlinkage int our_sys_open(const char* file, int flags, int mode)
+asmlinkage int hook_open(const char* pathname, int flags, int mode)
 {
-   printk("A file was opened\n");
-   return original_call(file, flags, mode);
+   return original_open(pathname, flags, mode);
 }
 
-static void disable_page_protection(void) {
+asmlinkage int hook_close(int fd)
+{
+   return original_close(fd);
+}
 
+asmlinkage off_t hook_lseek(int fd, off_t offset, int whence)
+{
+   return original_lseek(fd, offset, whence);
+}
+
+asmlinkage ssize_t hook_read(int fd, void *buf, size_t count)
+{
+    return original_read(fd, buf, count);
+}
+
+asmlinkage ssize_t hook_write(int fd, const void *buf, size_t count)
+{
+    return original_write(fd, buf, count);
+}
+
+static void disable_page_protection(void) 
+{
     unsigned long value;
     asm volatile("mov %%cr0,%0" : "=r" (value));
-    if (value & 0x00010000) {
-            value &= ~0x00010000;
-            asm volatile("mov %0,%%cr0": : "r" (value));
+    if (value & 0x00010000) 
+    {
+        value &= ~0x00010000;
+        asm volatile("mov %0,%%cr0": : "r" (value));
     }
 }
 
-static void enable_page_protection(void) {
-
+static void enable_page_protection(void) 
+{
     unsigned long value;
     asm volatile("mov %%cr0,%0" : "=r" (value));
-    if (!(value & 0x00010000)) {
-            value |= 0x00010000;
-            asm volatile("mov %0,%%cr0": : "r" (value));
+    if (!(value & 0x00010000)) 
+    {
+        value |= 0x00010000;
+        asm volatile("mov %0,%%cr0": : "r" (value));
     }
 }
 
-static int __init hello_init(void)
+static int __init diob_init(void)
 {
-    sys_call_table = (unsigned long*)0xffffffff80296f40;
-    printk(KERN_INFO "Hello world! We're up and running. syscall @ %p, __NR_open is %x\n", sys_call_table, __NR_open);
-    original_call = sys_call_table[__NR_open];
+    printk(KERN_INFO "diob_lkm up and running. syscall @ %p, __NR_open is %x\n", SYS_CALL_TABLE, __NR_open);
+    original_open = SYS_CALL_TABLE[__NR_open];
+    original_close = SYS_CALL_TABLE[__NR_close];
+    original_lseek = SYS_CALL_TABLE[__NR_lseek];
+    original_read = SYS_CALL_TABLE[__NR_read];
+    original_write = SYS_CALL_TABLE[__NR_write];
     disable_page_protection();
-    sys_call_table[__NR_open] = our_sys_open;
+    SYS_CALL_TABLE[__NR_open] = hook_open;
+    SYS_CALL_TABLE[__NR_close] = hook_close;
+    SYS_CALL_TABLE[__NR_lseek] = hook_lseek;
+    SYS_CALL_TABLE[__NR_read] = hook_read;
+    SYS_CALL_TABLE[__NR_write] = hook_write;
     enable_page_protection();
-    printk(KERN_INFO "Successfully set new __NR_open.\n");
-    return 0;	// Non-zero return means that the module couldn't be loaded.
+    printk(KERN_INFO "Successfully set up I/O hooks.\n");
+    return 0;
 }
 
-static void __exit hello_cleanup(void)
+static void __exit diob_cleanup(void)
 {
-    printk(KERN_INFO "Cleaning up module.\n");
     disable_page_protection();
-    sys_call_table[__NR_open] = original_call;
+    SYS_CALL_TABLE[__NR_open] = original_open;
+    SYS_CALL_TABLE[__NR_close] = original_close;
+    SYS_CALL_TABLE[__NR_lseek] = original_lseek;
+    SYS_CALL_TABLE[__NR_read] = original_read;
+    SYS_CALL_TABLE[__NR_write] = original_write;
     enable_page_protection();
-    printk(KERN_INFO "Successfully re-set old __NR_open.\n");
+    printk(KERN_INFO "Successfully removed I/O hooks.\n");
 }
 
-module_init(hello_init);
-module_exit(hello_cleanup);
+module_init(diob_init);
+module_exit(diob_cleanup);
